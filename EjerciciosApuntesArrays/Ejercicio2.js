7a54dfb367b25ec51ef681370e6ba933eaf687a0
Javito999 <146422988+Javito999@users.noreply.github.com>
2025-09-29 10:59:47 +0200

    ejercicios pa casa

---
+++ EjerciciosApuntesArrays/Ejercicio2.js
@@ -0,0 +1,16 @@
+let array = ["tela", "bies", "hilo",
+    "tijeras", "maquina de coser", "boton"
+];
+
+array.splice(1 ,1);
+
+array.splice(2, 0, "hilo torzal");
+
+array.splice(3,1, "cuter rotatorio", "mesa de corte");
+
+let array1 = array.splice(3, 3);
+
+let array2 = array.splice(3, 2);
+
+//ejercicio no terminado
+
---
+++ EjerciciosApuntesArrays/Ejercicio3.js
@@ -0,0 +1,10 @@
+let array = ["tela", "bies", "hilo", "hilo torzal",
+    "tijeras", "máquina de coser", "bptón"
+];
+
+array.sort(function(elem1, elem2){
+
+    elem1.
+
+
+});
