8333a90da1851b4649f0258f7a2c6fd20df982f2
Javito999 <146422988+Javito999@users.noreply.github.com>
2025-11-06 10:13:03 +0100

    tres en raya

---
+++ EjerciciosDOM_BOM/TresEnRaya/scripts/tresEnRaya.js
@@ -0,0 +1,37 @@
+function iniciarPartida(){
+
+    let boton = document.getElementById("iniciaPartida");
+
+
+    if(boton){
+
+    }else{
+        return;
+    }
+
+}
+function partida(){
+
+    iniciarPartida();
+
+    let jugador1 = document.getElementById("jugador1");
+    let jugador2 = document.getElementById("jugador2");
+
+    let arrayBidimensional = new Array(3);
+
+    for(let i = 0; 1 < 3; i++){
+        arrayBidimensional[i] = new Array(3);
+    }
+
+    for(let i = 0; i < 3; i++){
+
+        for(let j = 0; j < 3; j++){
+            arrayBidimensional[i][j];
+        }
+    }
+
+    if(jugador1)
+
+
+
+}
