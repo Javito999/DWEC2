73cc3619aa4334cbda839e370b058d2d11f12175
Javito999 <146422988+Javito999@users.noreply.github.com>
2025-11-12 13:25:32 +0100

    mas ejercicios

--- Ejemplo innerHTML y eventos/Ejemplo innerHTML y eventos/scripts/scripts.js
+++ Ejemplo innerHTML y eventos/Ejemplo innerHTML y eventos/scripts/scripts.js
@@ -1,3 +1,5 @@
+const { createElement } = require("react");
+
 window.onload = function () {
   document
     .getElementById("tablaDatos")
@@ -10,8 +12,16 @@ window.onload = function () {
 //Ejemplo Punto 1.7 de los apuntes
 function crearNuevaFila() {
   let numFilas = this.childElementCount;
-  let fila = "<tr id='" + numFilas + "'><td>" + (numFilas + 1) + "</td><td>" + (numFilas + 2) + "</td></tr>";
-  this.innerHTML += fila;
+  let fila = document.createElement("tr");
+  fila.setAttribute("id", numFilas);
+  for(let i = 0; i <= 2; i++){
+    let celda = document.createElement("td");
+    celda.innerText = numFilas + i;
+    fila.appendChild(celda);
+  }
+
+  
+  this.appenChild(fila);
   document.getElementById(numFilas).addEventListener(
     "click",
     (event) => {
