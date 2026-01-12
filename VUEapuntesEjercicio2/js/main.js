814da7e20042a72b7849254e2172744a03a8f052
Javito999 <146422988+Javito999@users.noreply.github.com>
2026-01-12 11:03:27 +0100

    VUE

---
+++ VUEapuntesEjercicio2/js/main.js
@@ -0,0 +1,22 @@
+const { createApp, ref, reactive } = Vue;
+
+const app = createApp({
+
+
+});
+
+
+const lista = createApp({
+    setup(){}
+
+    const personas = reactive([
+        {dni:12234422, nombre:'Paco', aplellidos:'Márquez Gómez'},
+        {dni:14563344, nombre:'Silvia', aplellidos:'Sánchez Gómez'},
+        {dni:23454422, nombre:'Fernando', aplellidos:'Estévez Rodriguez'},
+    ])
+
+    
+
+
+
+});
